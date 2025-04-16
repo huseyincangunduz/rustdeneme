a6ca00d5beb1c0c9c87b2455dfc7dca895180a00
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2025-04-16 13:05:13 +0300

    work2

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ use participant::*;
 use uuid::Uuid;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main()  {
     // let mut p_list: Vec<participant::Participant> = Vec::new();
     // p_list.push(participant::Participant {
     //     client_id: Uuid::new_v4(),
@@ -44,48 +44,20 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // Next up we create a TCP listener which will listen for incoming
     // connections. This TCP listener is bound to the address we determined
     // above and must be associated with an event loop.
-    let listener = TcpListener::bind(&addr).await?;
     println!("Listening on: {}", addr);
+    let (tx, rx) = mpsc::channel();
+    tokio::spawn(
+        async move {
+            tx.send(participant::Participant {
+                client_id: Uuid::new_v4(),
+                instance_name: String::new(),
+                subject_name: String::new(),
+                subject_type: SubjectType::EVENT
+            }).unwrap();
+        }
+    );
+    println!("msg {}", rx.recv().unwrap().client_id);
 
-    loop {
-        // Asynchronously wait for an inbound socket.
-        let (tx, rx) = mpsc::channel();
-        let (mut socket, _) = listener.accept().await?;
-
-        // And this is where much of the magic of this server happens. We
-        // crucially want all clients to make progress concurrently, rather than
-        // blocking one on completion of another. To achieve this we use the
-        // `tokio::spawn` function to execute the work in the background.
-        //
-        // Essentially here we're executing a new task to run concurrently,
-        // which will allow all of our clients to be processed concurrently.
-
-        tokio::spawn(async move {
-            let tx = tx.clone();
-            tx.send(31).unwrap();
-
-            let mut buf = vec![0; 1024];
-           
-            loop {
-                let n = socket
-                    .read(&mut buf)
-                    .await
-                    .expect("failed to read data from socket");
-
-                if n == 0 {
-                    return;
-                }
-
-                socket
-                    .write_all(&buf[0..n])
-                    .await
-                    .expect("failed to write data to socket");
-            }
-        });
-        // let a = midone.clone();
-        // print!("{}", a.subject_name);
-        print!("msg {}", rx.recv().unwrap());
-    }
 }
 // async fn main() {
 
